e5b5b6651e9c91310a309d926b49463c90d4962b
morrellj <jamie.morrell@paddypowerbetfair.com>
2017-12-16 20:29:51 +0000

    node version

---
+++ slave.js
@@ -0,0 +1,18 @@
+var Discovery = require('udp-discovery').Discovery;
+var discover = new Discovery();
+ 
+ discover.on('available', function(name, data, reason) {
+   console.log('available ',name);
+     console.log('data',data);
+       console.log('reason',reason);
+         var obj = {a: 1, b: '2', c: true, d: {e: 333}};
+	   discover.sendEvent('Hello', obj);
+	    
+	      console.log(name,':','available:',reason);
+	        console.log(data);
+		});
+		 
+		 discover.on('unavailable', function(name, data, reason) {
+		   console.log(name,':','unavailable:',reason);
+		     console.log(data);
+		     });
